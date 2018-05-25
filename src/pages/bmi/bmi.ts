32d50cc71f11ee83778db6a93315b34e3e67ee12
Aatif Zaidi <aatifsuntech@gmail.com>
2018-05-25 21:49:11 +0530

    First commit

---
+++ src/pages/bmi/bmi.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { BmiProvider } from '../../providers/bmi/bmi';
+import { BMI } from '../../models/bmi.models';
+
+@IonicPage()
+@Component({
+  selector: 'page-bmi',
+  templateUrl: 'bmi.html',
+})
+export class BmiPage {
+  height: number;
+  weight: number;
+  BMI: BMI;
+
+  constructor(private bmiProvider: BmiProvider) {}
+
+  calculateBmi(){
+    this.BMI = this.bmiProvider.calculateBMI(this.height, this.weight);
+
+  }
+
+}
